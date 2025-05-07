b6f8b3822a9caa6916751633ebdb54a891e06e05
Алексей <100560676+Spectyhome@users.noreply.github.com>
2025-05-07 15:03:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('5.html')
+    return render_template('xfive.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
